6e948346461dd3151ae2e40d3df3a449fe605d68
Otabek1990 <fozilovotabek424@gmail.com>
2024-09-18 08:37:33 +0500

    feat/status added to card

--- src/constants/homeCardStatusses.js
+++ src/constants/homeCardStatusses.js
@@ -1,14 +1,22 @@
 export const homeCardStatusses={
     ending:{
+        bgColor:"#FEEDEC",
+        color:"#DC1010",
         titleRu:"Заканчивается"
     },
     new:{
-        titleRu:"Новый сбор"
+        titleRu:"Новый сбор",
+        bgColor:"#E7F4EE",
+        color:"#40C979"
     },
     ended:{
-        titleRu:"Завершено"
+        titleRu:"Завершено",
+        bgColor:"#FF8A00",
+        color:"#FFFFFF"
     },
     normal:{
-        titleRu:""
+        titleRu:"",
+        bgColor:"transparent",
+        color:"transparent"
     }
 }
